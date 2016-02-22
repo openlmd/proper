cd860ca2343579eca52df3aab161695eefebbc3b
Baltasar Lodeiro <baltasar.lodeiro@aimen.es>
2016-02-22 16:09:03 +0100

    Archivos iniciales
    Former-commit-id: 7855ad6596e6facb9adf17a351bd994cc53ad6a4 [formerly 2a40864a3b16f8fcf3461f4506d16b78ee60b4de]
    Former-commit-id: cd78c39f9086e7a3bbc81f329eabe1224d32f21a

---
+++ proper_abb/src/abb/server_robot.py
@@ -0,0 +1,48 @@
+import yaml
+from abb import Robot
+
+class ServerRobot(Robot):
+    def __init__(self):
+        Robot.__init__(self)
+
+    def connect(self, ip):
+        #self.robot.init_ant('172.20.0.32')
+        self.connect_motion((ip, 5000))
+        self.set_units('millimeters', 'degrees')
+        self.set_tool()
+        self.set_workobject()
+        self.set_speed()
+        self.set_zone()
+
+    def disconnect(self):
+        self.close()
+
+    def workobject(self, work_obj):
+        self.set_workobject(work_obj)
+
+    def tool(self, tool_pose):
+        self.set_tool(tool_pose)
+
+    def configure(self, filename):
+        print filename
+
+    def move(self, pose):
+        self.set_cartesian(pose)
+
+    def speed(self, speed):
+        self.set_speed([speed, 500, 50, 50])
+
+    def zone(self, zone):
+        self.set_zone(zone_key=zone)
+
+
+if __name__ == '__main__':
+    server_robot = ServerRobot()
+    server_robot.connect('172.20.0.32')
+    # server_robot.workobject([[1.655, -0.087, 0.932], [1, 0, 0, 0]])
+    # server_robot.tool([[0.216, -0.022, 0.474], [0.5, 0, -0.866025, 0]])
+    server_robot.speed(50)
+    server_robot.move([[1000, 0, 1000], [0, 0, 1, 0]])
+    server_robot.speed(100)
+    server_robot.move([[900, 0, 900], [0, 0, 1, 0]])
+    server_robot.disconnect()
