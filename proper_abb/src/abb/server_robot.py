a73022b089ba0545745bc27c016333a26bd35cb8
Baltasar Lodeiro <baltasar.lodeiro@aimen.es>
2016-04-28 16:11:52 +0200

    Laser test .jas, and robot_server bugfix
    Former-commit-id: ad41c7aec6ff8f375987d75b27d1c8add5d58d51 [formerly 48fa335d173de98336f1428f8341602e1a203e42]
    Former-commit-id: 0d0278e72755193f0d3bc0ec20a9ae438d2a838c

--- proper_abb/src/abb/server_robot.py
+++ proper_abb/src/abb/server_robot.py
@@ -141,14 +141,14 @@ class ServerRobot(Robot):
                 elif dato == 'get_pose':
                     return self.get_cartesian()
                 elif dato == 'wait_time':
-                    return self.wait_time(comando_json[dato])
-                elif dato == 'wait_laserbeam':
-                    return self.wait_input((comando_json[dato], 0))
+                    self.wait_time(comando_json[dato])
+                elif dato == 'wait_standby':
+                    self.wait_input(comando_json[dato], 0)
                 elif dato == 'wait_generalfault':
-                    return self.wait_input((comando_json[dato], 1))
-                elif dato == 'rf_main':
+                    self.wait_input(comando_json[dato], 1)
+                elif dato == 'laser_main':
                     self.set_digital((comando_json[dato], 2))
-                elif dato == 'rf_standby':
+                elif dato == 'laser_standby':
                     self.set_digital((comando_json[dato], 3))
                 elif dato == 'weldgas':
                     self.set_digital((comando_json[dato], 4))
